38bcddafcef8fdb80a98180ca9e16b5a684231c0
Allusion-Steven <steven@allusionsoftware.com>
2021-11-13 00:12:43 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+// Import stylesheets
+import './style.css';
+import 'firebase'
+// html element references
+const 
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+
+function OBJECT_MODEL(){
+  let object_model = {
+    //test data
+    key_0: `value_0`,
+    key_1: `value_1`,
+    key_2: `value_2`,
+    key_3: `value_3`,
+    key_4: `value_4`,
+  };
+}
